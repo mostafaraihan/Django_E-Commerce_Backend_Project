21f7ef9f0c454657155d89ba5596cdf015b1d1ff
Mostafa Raihan <raihan.invite@gmail.com>
2025-12-20 12:25:45 +0600

    Brand Model Create

--- ecommerce/models.py
+++ ecommerce/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Brand(models.Model):
+    brandName = models.CharField(max_length=100)
+    brandImg = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
