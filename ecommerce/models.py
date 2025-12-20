274a34d24d070ff615076bb2a55e4edd937e9092
Mostafa Raihan <raihan.invite@gmail.com>
2025-12-20 12:43:51 +0600

    Product Wish Model Create

--- ecommerce/models.py
+++ ecommerce/models.py
@@ -116,3 +116,10 @@ class ProductReview(models.Model):
 
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+class ProductWish(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
