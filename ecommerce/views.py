ece02fa71c2c7709e70c21e8b44b0a095bdd945f
Mostafa Raihan <raihan.invite@gmail.com>
2025-12-20 12:12:06 +0600

    template index.html file create

--- ecommerce/views.py
+++ ecommerce/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
 
 def index(request):
-    return HttpResponse("Django Ecommerce Backend Project")
+    return render(request,'index.html')
