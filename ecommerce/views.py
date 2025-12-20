1af7f212317f0b1cd6e726fe2e8fe94a0a3548a7
Mostafa Raihan <raihan.invite@gmail.com>
2025-12-20 13:04:13 +0600

    views function create

--- ecommerce/views.py
+++ ecommerce/views.py
@@ -3,3 +3,35 @@ from django.shortcuts import render
 
 def index(request):
     return render(request,'index.html')
+
+
+def brand_list(request):
+    pass
+
+
+def category_list(request):
+    pass
+
+
+def product_by_category(request, category_id):
+    pass
+
+
+def product_by_remark(request, remark):
+    pass
+
+
+def product_slider_list(request):
+    pass
+
+
+def product_by_keyword(request):
+    pass
+
+
+def product_details(request, product_id):
+    pass
+
+
+def product_list(request):
+    pass
