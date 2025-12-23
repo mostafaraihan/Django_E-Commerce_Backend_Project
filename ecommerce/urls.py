035fdcdc1584944a92d62025dca8e68f483acf4c
Mostafa Raihan <raihan.invite@gmail.com>
2025-12-23 18:52:10 +0600

    urls add

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -2,5 +2,16 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index)
+    path('', views.index),
+    path('brands/', views.brand_list),
+    path('categories/', views.category_list),
+
+    path('products/category/<int:category_id>/', views.product_by_category),
+    path('products/remark/<str:remark>/', views.product_by_remark),
+    path('products/brand/<int:brand_id>/', views.product_by_brand),
+
+    path('products/sliders/', views.product_slider_list),
+    path('products/search/', views.product_by_keyword),
+
+    path('products/<int:product_id>/', views.product_details),
 ]
