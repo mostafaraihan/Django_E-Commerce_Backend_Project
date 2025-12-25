07f995e80d5abe5a67166a1e05f21b00bc8b4268
Mostafa Raihan <raihan.invite@gmail.com>
2025-12-25 23:17:41 +0600

    apps

--- ecommerce/apps.py
+++ ecommerce/apps.py
@@ -3,3 +3,10 @@ from django.apps import AppConfig
 
 class EcommerceConfig(AppConfig):
     name = 'ecommerce'
+
+class PollsConfig(AppConfig):
+    name = 'polls'
+    icon = 'fa fa-square-poll-vertical'  # FontAwesome icon
+    divider_title = "Apps"  # Section divider title
+    priority = 0  # Sidebar ordering (higher = top)
+    hide = False  # Hide from sidebar
